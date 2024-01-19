3292f922a3b59c08b08d8f34e6d42cb198c81d02
Metin Kaya <metin.kaya@arm.com>
2024-01-19 16:15:14 +0000

    tools/buildroot: Add support for generating Linux target system images
    Integrate buildroot into devlib in order to ease building kernel and
    root filesystem images via 'generate-kernel-initrd.sh' helper script.
    
    As its name suggests, the script builds kernel image which also includes
    an initial RAM disk per default config files located under
    configs/<arch>/.
    
    Provide config files for buildroot and Linux kernel as well as a
    post-build.sh script which tweaks (e.g., allowing root login on SSH)
    target's root filesystem.
    
    doc/tools.rst talks about details of kernel and rootfs configuration.
    
    Signed-off-by: Metin Kaya <metin.kaya@arm.com>

---
+++ tools/buildroot/configs/post-build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -eux
+
+# Enable root login on SSH
+sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' "${TARGET_DIR}/etc/ssh/sshd_config"
+
+# Increase the number of available channels so that devlib async code can
+# exploit concurrency better.
+sed -i 's/#MaxSessions.*/MaxSessions 100/' "${TARGET_DIR}/etc/ssh/sshd_config"
+sed -i 's/#MaxStartups.*/MaxStartups 100/' "${TARGET_DIR}/etc/ssh/sshd_config"
---
+++ tools/buildroot/generate-kernel-initrd.sh
@@ -0,0 +1,123 @@
+#! /bin/bash
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2024, ARM Limited and contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Forked from LISA/tools/lisa-buildroot-create-rootfs.
+#
+
+set -eu
+
+BUILDROOT_URI="git://git.busybox.net/buildroot"
+BUILDROOT_VERSION="2023.11.1"
+BUILDROOT_DIR="$(dirname "$0")/buildroot-v${BUILDROOT_VERSION}"
+
+ARCH="aarch64"
+KERNEL_IMAGE_NAME="Image"
+
+function print_usage
+{
+	echo "Usage: ${0} [options]"
+	echo "	options:"
+	echo "		-a: set arch (default is aarch64, x86_64 is also supported)"
+	echo "		-p: purge buildroot to force a fresh build"
+	echo "		-h: print this help message"
+}
+
+function set_arch
+{
+	if [[ "${1}" == "aarch64" || "${1}" == "x86_64" ]]; then
+		ARCH=${1}
+		KERNEL_IMAGE_NAME="bzImage"
+		return 0
+	fi
+
+	return 1
+}
+
+while getopts "ahp" opt; do
+	case ${opt} in
+	a)
+		shift
+		if ! set_arch "${1}"; then
+			echo "Invalid arch \"${1}\"."
+			exit 1
+		fi
+		;;
+	p)
+		rm -rf "${BUILDROOT_DIR}"
+		exit 0
+		;;
+	h)
+		print_usage
+		exit 0
+		;;
+	*)
+		print_usage
+		exit 1
+		;;
+	esac
+done
+
+# Execute function for once
+function do_once
+{
+	FILE="${BUILDROOT_DIR}/.devlib_${1}"
+	if [ ! -e "${FILE}" ]; then
+		eval "${1}"
+		touch "${FILE}"
+	fi
+}
+
+function br_clone
+{
+	git clone ${BUILDROOT_URI} "${BUILDROOT_DIR}"
+}
+
+function br_checkout_version
+{
+	pushd "${BUILDROOT_DIR}" >/dev/null
+	git checkout -b ${BUILDROOT_VERSION} ${BUILDROOT_VERSION}
+	popd >/dev/null
+}
+
+function br_apply_config
+{
+	mkdir -p "${BUILDROOT_DIR}/board/arm-power/${ARCH}/"
+	cp -f "configs/post-build.sh" "${BUILDROOT_DIR}/board/arm-power/"
+	cp -f "configs/${ARCH}/arm-power_${ARCH}_defconfig" "${BUILDROOT_DIR}/configs/"
+	cp -f "configs/${ARCH}/linux.config" "${BUILDROOT_DIR}/board/arm-power/${ARCH}/"
+
+	make -C "${BUILDROOT_DIR}" "arm-power_${ARCH}_defconfig"
+}
+
+function br_build
+{
+	pushd "${BUILDROOT_DIR}" >/dev/null
+	make
+	popd >/dev/null
+}
+
+
+do_once br_clone
+
+do_once br_checkout_version
+
+do_once br_apply_config
+
+br_build
+
+echo "Kernel image \"${BUILDROOT_DIR}/output/images/${KERNEL_IMAGE_NAME}\" is ready."
